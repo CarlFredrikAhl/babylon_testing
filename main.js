829219f63eb26c12894364ec8373acac535e1bb5
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2021-12-03 12:00:38 +0100

    Created car

--- main.js
+++ main.js
@@ -6,128 +6,55 @@ function createScene() {
 
     const scene = new BABYLON.Scene(engine);
 
-    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(-20, 10, -20), scene);
+    const camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(1, 5, -3), scene);
     camera.attachControl(canvas, true);
-    camera.rotation.y = BABYLON.Tools.ToRadians(50);
+    camera.rotation.y = BABYLON.Tools.ToRadians(10);
     camera.rotation.x = BABYLON.Tools.ToRadians(20);
-    
+    camera.speed = 0.5;
+
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
 
-    //OBJECTS
-    const ground = buildGround(30, 30);
-    
+    //Load village
+    BABYLON.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "village.glb");
+
     //SOUND
-    const sound = new BABYLON.Sound("background_music", "emotional_background_music.wav",
-    scene, null, { loop:true, autoplay:true });
+    // const sound = new BABYLON.Sound("background_music", "emotional_background_music.wav",
+    // scene, null, { loop:true, autoplay:true });
 
+    //Creating car
+
+    //base
+    const outline = [
+        new BABYLON.Vector3(-0.3, 0, -0.1),
+        new BABYLON.Vector3(0.2, 0, -0.1),
+    ]
+
+    //top
+    outline.push(new BABYLON.Vector3(0, 0, 0.1));
+    outline.push(new BABYLON.Vector3(-0.3, 0, 0.1));
+
+    const car = BABYLON.MeshBuilder.ExtrudePolygon("car", { shape: outline, depth: 0.2 });
+    car.position.y = 3;
+    car.rotation.x = BABYLON.Tools.ToRadians(270);
+
+    //Creating wheel
+
+    const wheelLB = BABYLON.MeshBuilder.CreateCylinder("wheelLB", {diameter: 0.125, height: 0.05});
+    wheelLB.parent = car;
+    wheelLB.position.x = -0.2;
+    wheelLB.position.y = -0.2;
+    wheelLB.position.z = -0.1;
+
+    const wheelLF = wheelLB.clone("wheelLF");
+    wheelLF.position.x = 0.1;
     
-    createVillage();
+    const wheelRB = wheelLB.clone("wheelRB");
+    wheelRB.position.y = 0;
+    
+    const wheelRF = wheelLF.clone("wheelRF");
+    wheelRF.position.y = 0;
 
-    BABYLON.GLTF2Export.GLBAsync(scene, "village.glb").then((glb) => {
-        glb.downloadFiles();
-    });
     return scene;
-}
-
-function createVillage() {
-
-    //const box = buildBox(2);
-    const regularHouse = buildHouse(1);
-    const semiHouse = buildHouse(2);
-    semiHouse.position.z = 3;
-
-    //Create village from the first houses
-    const places = []; // [house type, rotation, x, z]
-    places.push([1, BABYLON.Tools.ToRadians(20), -3, -5]);
-    places.push([2, BABYLON.Tools.ToRadians(45), -3, 12]);
-    places.push([1, BABYLON.Tools.ToRadians(50), -10, 5]);
-    places.push([1, BABYLON.Tools.ToRadians(30), 12, 5]);
-    places.push([2, BABYLON.Tools.ToRadians(60), -7, -5]);
-    places.push([2, BABYLON.Tools.ToRadians(70), -3, -8]);
-    places.push([2, BABYLON.Tools.ToRadians(80), 9, 5]);
-    places.push([1, BABYLON.Tools.ToRadians(90), 2,  10]);
-    places.push([1, BABYLON.Tools.ToRadians(100), -13, -5]);
-    places.push([1, BABYLON.Tools.ToRadians(110), -12, -12]);
-    places.push([2, BABYLON.Tools.ToRadians(112), 4, -5]);
-    places.push([1, BABYLON.Tools.ToRadians(113), -3, 1]);
-    places.push([1, BABYLON.Tools.ToRadians(180), -10, -5]);
-    
-    const houses = [];
-
-    for(let i = 0; i < places.length; i++) {
-        
-        if(places[i][0] == 1) {
-            houses[i] = regularHouse.createInstance("house" + i);
-        } else {
-            houses[i] = semiHouse.createInstance("house" + i);
-        }
-        houses[i].rotation.y = places[i][1];
-        houses[i].position.x = places[i][2];
-        houses[i].position.z = places[i][3];
-    }
-}
-
-function buildHouse(width) {
-    const box = buildBox(width);
-    const roof = buildRoof(width);
-
-    return BABYLON.Mesh.MergeMeshes([box, roof], true, false, null, false, true);
-}
-
-function buildBox (width){
-    
-    const boxMat = new BABYLON.StandardMaterial('boxMat');
-    
-    const faceUV = [];
-    
-    if(width == 1) {
-        
-        boxMat.diffuseTexture = new BABYLON.Texture("cubehouse.png");
-
-        faceUV[3] = new BABYLON.Vector4(0, 0, 0.25, 1); //front
-        faceUV[1] = new BABYLON.Vector4(0.25, 0, 0.5, 1); //right
-        faceUV[2] = new BABYLON.Vector4(0.5, 0, 0.75, 1); //back
-        faceUV[0] = new BABYLON.Vector4(0.75, 0, 1, 1); //left
-    
-    } else {
-        
-        boxMat.diffuseTexture = new BABYLON.Texture("semihouse.png");
-
-        faceUV[0] = new BABYLON.Vector4(0.6, 0.0, 1.0, 1.0); //rear face
-        faceUV[1] = new BABYLON.Vector4(0.0, 0.0, 0.4, 1.0); //front face
-        faceUV[2] = new BABYLON.Vector4(0.4, 0, 0.6, 1.0); //right side
-        faceUV[3] = new BABYLON.Vector4(0.4, 0, 0.6, 1.0); //left side
-    }
-
-    const box = BABYLON.MeshBuilder.CreateBox("box", { width:width, faceUV: faceUV, wrap:true});
-    box.material = boxMat;
-    box.position.y = 0.5;
-
-    return box;
-}
-
-function buildRoof(width) {
-    
-    const roofMat = new BABYLON.StandardMaterial('roofMat');
-    roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg");
-
-    const roof = BABYLON.MeshBuilder.CreateCylinder('roof', {diameter:1.3, height:1.2, tessellation:3});
-    roof.rotation.z = BABYLON.Tools.ToRadians(90);
-    roof.material = roofMat;
-    roof.scaling.y = width;
-    roof.position.y = 1.22;
-    roof.scaling.x = 0.75;
-
-    return roof;
-}
-
-function buildGround(width, height) {
-    
-    const groundMat = new BABYLON.StandardMaterial("groundMat");
-    groundMat.diffuseColor = new BABYLON.Color3.Green;
-
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width:width, height:height});
-    ground.material = groundMat;
 }
 
 const scene = createScene();
